51fc4c2d91ad729e5cb4808a62c61c6ca8aaafb8
Mort <morteza.tourani@gmail.com>
2021-02-06 16:11:15 +0300

    Add icons

---
+++ src/components/icons/Plus.tsx
@@ -0,0 +1,19 @@
+import { FC, SVGProps } from 'react'
+
+export const Plus: FC<SVGProps<SVGSVGElement>> = props => (
+  <svg
+    {...props}
+    width='14'
+    height='14'
+    viewBox='0 0 14 14'
+    fill='none'
+    xmlns='http://www.w3.org/2000/svg'
+  >
+    <path
+      fill-rule='evenodd'
+      clip-rule='evenodd'
+      d='M14 8H8V14H6V8H0V6H6V0H8V6H14V8Z'
+      fill='currentcolor'
+    />
+  </svg>
+)
---
+++ src/components/icons/index.ts
@@ -0,0 +1,5 @@
+export * from './ChevronDown'
+export * from './Close'
+export * from './Minus'
+export * from './Plus'
+export * from './Search'
